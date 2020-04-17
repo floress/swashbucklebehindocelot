eadc05099544cfe2d0d1b16d4d9ece3df384ab52
Sebastian Flores <floress@gmail.com>
2020-04-17 17:08:39 -0300

    first commit

---
+++ src/SoFtware.Api1/Startup.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace SoFtware.Api1
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddMvcCore()
+            //    .AddDataAnnotations()
+            //    .AddApiExplorer();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Swashbuckle Behind Proxy",
+                    Version = "v1"
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My api v1");
+            });
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
+            });
+        }
+    }
+}
---
+++ src/SoFtware.Ocelot/SwaggerBehindProxyMiddleware.cs
@@ -0,0 +1,127 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SoFtware.Ocelot
+{
+    public class SwaggerBehindProxyMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+        private string _swaggerPrefix = "swagger";
+        private string _swaggerDocName = "v1";
+
+        public SwaggerBehindProxyMiddleware(RequestDelegate next,
+            ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _logger = loggerFactory.CreateLogger<SwaggerBehindProxyMiddleware>();
+        }
+
+        private string IndexUri => $"/{_swaggerPrefix}/index.html";
+        private string JsonUri => $"/{_swaggerPrefix}/{_swaggerDocName}/swagger.json";
+        private string[] PrefixSegments => _swaggerPrefix.Trim('/')
+                                            .Split('/')
+                                            .Select(s => s.Trim('/')).ToArray();
+
+        public async Task Invoke(HttpContext context)
+        {
+            var path = context.Request.Path;
+            if (path.HasValue && (path.Value.EndsWith(IndexUri) ||
+                                  path.Value.EndsWith(JsonUri)))
+            {
+                var requestSegments = context.Request.Path.Value.Trim('/')
+                    .Split("/")
+                    .Select(s => s.Trim('/'))
+                    .ToArray();
+
+                var miroserviceName = requestSegments.Reverse()
+                    .Skip(PrefixSegments.Length + 1) //Segments and file 
+                    .Take(1)
+                    .SingleOrDefault();
+
+                var referenceToOriginalBody = context.Response.Body;
+
+                await using var responseBody = new MemoryStream();
+
+                context.Response.Body = responseBody;
+
+                await _next(context);
+
+                async Task<string> ReadResponse(HttpResponse resp)
+                {
+                    resp.Body.Seek(0, SeekOrigin.Begin);
+                    var text = await new StreamReader(resp.Body).ReadToEndAsync();
+                    resp.Body.Seek(0, SeekOrigin.Begin);
+                    return text;
+                }
+
+                var response = await ReadResponse(context.Response);
+
+                byte[] buffer;
+
+                if (path.Value.EndsWith(JsonUri))
+                {
+                    // frontend reading json
+                    _logger.LogInformation($"Recreating swagger.json for {miroserviceName} service");
+                    await using var ms = new MemoryStream();
+                    using var wr = new JsonTextWriter(new StreamWriter(ms))
+                    {
+                        Indentation = 2, IndentChar = ' ', Formatting = Formatting.Indented
+                    };
+                    var o = JObject.Parse(response);
+                    wr.WriteStartObject();
+                    foreach (var jProperty in o.Children<JProperty>())
+                    {
+                        if (jProperty.Name == "paths")
+                        {
+                            wr.WritePropertyName(jProperty.Name);
+                            wr.WriteStartObject();
+                            foreach (var pathProperty in jProperty.Value.Children())
+                            {
+                                var originalPath = (JProperty)pathProperty;
+                                wr.WritePropertyName($"/{requestSegments[0]}{originalPath.Name}");
+                                originalPath.Value.WriteTo(wr);
+                            }
+                            wr.WriteEndObject();
+                        }
+                        else
+                        {
+                            jProperty.WriteTo(wr);
+                        }
+                    }
+                    wr.WriteEndObject();
+                    wr.Flush();
+                    wr.Close();
+
+                    buffer = ms.ToArray();
+                } 
+                else if (path.Value.EndsWith(IndexUri) && response.Contains(JsonUri))
+                {
+                    // replace url swagger configObject configuration
+                    response = response.Replace($"/{_swaggerPrefix}/{_swaggerDocName}/swagger.json", $"/{requestSegments[0]}/{_swaggerPrefix}/{_swaggerDocName}/swagger.json");
+                    buffer = Encoding.UTF8.GetBytes(response);
+                }
+                else
+                {
+                    // to prevents undesiderable errors
+                    return;
+                }
+
+                context.Response.Headers.ContentLength = buffer.Length;
+                await referenceToOriginalBody.WriteAsync(buffer, 0, buffer.Length);
+                responseBody.Seek(0, SeekOrigin.Begin);
+                await responseBody.CopyToAsync(referenceToOriginalBody);
+            }
+            else
+            {
+                await _next(context);
+            }
+        }
+    }
+}
